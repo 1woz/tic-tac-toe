3f98aadac2a097600fea6849c89cff522fd2e3ad
Iwona Ozimek <az0r.alq@hotmail.com>
2022-10-15 21:27:34 +0200

    completed game logic

---
+++ src/components/Game.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { calculateWinner } from "../helpers";
+import Board from "./Board";
+
+const styles = {
+  width: "200px",
+  margin: "20px auto",
+};
+
+const Game = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(history[stepNumber]);
+
+  const handleClick = (i) => {
+    const timeInHistory = history.slice(0, stepNumber + 1);
+    const current = timeInHistory[stepNumber];
+    const squares = [...current];
+    // if user clicks on square that is taken or if game is won, return
+    if (winner || squares[i]) return;
+    // add X or O in the clicked square
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory([...timeInHistory, squares]);
+    setStepNumber(timeInHistory.length);
+    setXisNext(!xIsNext); // return opposite value
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXisNext(step % 2 === 0);
+  };
+
+  const renderMoves = () =>
+    history.map((_step, move) => {
+      const destination = move ? `Move:${move}` : "START";
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{destination}</button>
+        </li>
+      );
+    });
+
+  return (
+    <>
+      <Board squares={history[stepNumber]} onClick={handleClick} />
+      <div style={styles}>
+        <p>
+          {winner
+            ? "Winner: " + winner
+            : "Next Player: " + (xIsNext ? "X" : "O")}
+        </p>
+        {renderMoves()}
+      </div>
+    </>
+  );
+};
+
+export default Game;
